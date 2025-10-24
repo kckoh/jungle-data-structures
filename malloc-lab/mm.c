5e25ac24d7bddb912579d225af587f384ba9911f
kckoh <kyungcheolkoh@gmail.com>
2025-10-24 17:22:01 +0900

    finish implicit with first fit

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -181,21 +181,24 @@ while(1){
     return cur;
 }
 
+// this was the issue
 static void place(void *bp, size_t asize){
-    /*
-    Mark that block as allocated (set the alloc bit to 1).
-    Optionally split the block if it’s bigger than needed.
-    The front portion becomes the allocated block.
-    The remainder becomes a smaller free block.
-    */
-    size_t cur_size = GET_SIZE(HDRP(bp));
+    size_t csize = GET_SIZE(HDRP(bp));
 
-    // optionally add split
 
-    PUT(HDRP(bp), PACK(asize, 1));
-    PUT(FTRP(bp), PACK(asize, 1));
-
+    if ((csize - asize) >= (2*DSIZE)) {  // Minimum block size
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize-asize, 0));
+        PUT(FTRP(bp), PACK(csize-asize, 0));
+    }
+    else {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
 }
+
 
 
 /*
